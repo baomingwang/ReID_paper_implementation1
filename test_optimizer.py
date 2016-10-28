d16a6319442bc46fe0e635828df792c9aa875ae7
kevin <wangbaoming0215@163.com>
2016-10-28 22:02:09 +0800

    Update test_optimizer.py

--- test_optimizer.py
+++ test_optimizer.py
@@ -2,10 +2,61 @@ import numpy as np
 np.random.seed(1234)
 from keras.models import Model
 from keras.layers import Input,Dense,Convolution2D,Activation,MaxPooling2D,Flatten,merge
-from sgd_paper import SGD_paper
+#from sgd_paper import SGD_paper
+from keras.optimizers import *
 from keras.utils import np_utils
 import pickle
 from keras import backend as K
+
+class SGD_paper(SGD):
+  '''Stochastic gradient descent, with support for momentum,
+    learning rate decay, and Nesterov momentum.
+
+    # Arguments
+        lr: float >= 0. Learning rate.
+        momentum: float >= 0. Parameter updates momentum.
+        decay: float >= 0. Learning rate decay over each update.
+        nesterov: boolean. Whether to apply Nesterov momentum.
+    '''
+    def __init__(self, lr=0.01, momentum=0., decay=0.,
+                 nesterov=False, **kwargs):
+        super(SGD, self).__init__(**kwargs)
+        self.__dict__.update(locals())
+        self.iterations = K.variable(0.)
+        self.lr = K.variable(lr)
+        self.momentum = K.variable(momentum)
+        self.decay = K.variable(decay)
+        self.inital_decay = decay
+
+    def get_updates(self, params, constraints, loss):
+        grads = self.get_gradients(loss, params)
+        self.updates = []
+
+        lr = self.lr
+        if self.inital_decay > 0:
+            lr *= (1. / (1. + self.decay * self.iterations)) ** 0.75
+            self.updates .append(K.update_add(self.iterations, 1))
+
+        # momentum
+        shapes = [K.get_variable_shape(p) for p in params]
+        moments = [K.zeros(shape) for shape in shapes]
+        self.weights = [self.iterations] + moments
+        for p, g, m in zip(params, grads, moments):
+            v = self.momentum * m - lr * g  # velocity
+            self.updates.append(K.update(m, v))
+
+            if self.nesterov:
+                new_p = p + self.momentum * v - lr * g
+            else:
+                new_p = p + v
+
+            # apply constraints
+            if p in constraints:
+                c = constraints[p]
+                new_p = c(new_p)
+
+            self.updates.append(K.update(p, new_p))
+        return self.updates
 
 a1 = Input(shape=(128,64,3))
 b1 = Input(shape=(128,64,3))
