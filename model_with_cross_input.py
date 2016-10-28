0a6c623523cee9e5f8a6095442de93142585255a
kevin <wangbaoming0215@163.com>
2016-10-28 16:01:45 +0800

    Update model_with_cross_input.py

--- model_with_cross_input.py
+++ model_with_cross_input.py
@@ -63,7 +63,7 @@ def cross_input(X):
     
 def cross_input_shape(input_shapes):
     input_shape = input_shapes[0]
-    return (input_shape[0],input_shape[1] * 5,input_shape[2] * 5,input_shape[3])
+    return (input_shape[0],input_shape[1] * 5,input_shape[2] * 5,input_shape[3]*2)
     
     
 a1 = Input(shape=(128,64,3))
