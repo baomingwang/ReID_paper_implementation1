1970dc700bd66f198bbe49b14a397faf2874128f
kevin <wangbaoming0215@163.com>
2016-10-28 10:43:38 +0800

    Update reid_model.py

--- reid_model.py
+++ reid_model.py
@@ -13,10 +13,32 @@ from keras.utils import np_utils
 from input_generator import DataSet
 from input_generator import load_from_file
 import pickle
-
+from keras import backend as K
 
 the_filename = 'data_by_path.pkl'
 dir_path = '/home/ubuntu/dataset/market1501/boundingboxtrain'
+
+def cross_input(X):
+    tensor_left = X[0]
+    tensor_right = X[1]
+    x_length = K.int_shape(tensor_left)[1]
+    y_length = K.int_shape(tensor_left)[2]
+    cross_y = []
+    cross_x = []
+    tensor_left_padding = K.spatial_2d_padding(tensor_left,padding=(2,2))
+    tensor_right_padding = K.spatial_2d_padding(tensor_right,padding=(2,2))
+    for i_x in range(2, x_length + 2):
+        for i_y in range(2, y_length + 2):
+            cross_y.append(tensor_left_padding[:,i_x-2:i_x+3,i_y-2:i_y+3,:] 
+                         - tensor_right_padding[:,i_x-2:i_x+3,i_y-2:i_y+3,:])
+        cross_x.append(K.concatenate(cross_y,axis=2))
+        cross_y = []
+    cross_out = K.concatenate(cross_x,axis=1)
+    return K.abs(cross_out)
+    
+def cross_input_shape(input_shapes):
+    input_shape = input_shapes[0]
+    return (input_shape[0],input_shape[1] * 5,input_shape[2] * 5,input_shape[3])
 
 a1 = Input(shape=(128,64,3))
 b1 = Input(shape=(128,64,3))
@@ -34,7 +56,7 @@ a6 = Activation('relu')(a5)
 b6 = Activation('relu')(b5)
 a7 = MaxPooling2D(dim_ordering='tf')(a6)
 b7 = MaxPooling2D(dim_ordering='tf')(b6)
-c1 = merge([a7,b7])
+c1 = merge([a7,b7], mode=cross_input, cross_input_shape)
 c2 = Convolution2D(25,5,5,dim_ordering='tf',activation='relu')(c1)
 c3 = MaxPooling2D((2,2),dim_ordering='tf')(c2)
 c4 = Convolution2D(25,3,3,dim_ordering='tf',activation='relu')(c3)
